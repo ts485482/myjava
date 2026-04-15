e97bf42c651cefe25a3b0e29d10807795b957fa3
leetaeshin <ts485482@gmail.com>
2026-04-15 14:09:32 +0900

    java

---
+++ leetaeshin/src/ch06/sec04/Car.java
@@ -0,0 +1,34 @@
+package ch06.sec04;
+
+public class Car {
+    //필드
+    int gas;
+
+    //생성자
+
+    //메소드
+    void setGas(int gas){
+        this.gas = gas;
+    }
+
+    boolean isLeftGas(){
+        if(gas == 0){
+            System.out.println("gas가 부족합니다.");
+            return false;
+        }
+        System.out.println("gas가 남아있습니다.");
+        return true;
+    }
+
+    void run(){
+        while(true){
+            if(gas>0){
+                System.out.println("달립니다. (gas 잔량 : " + gas + ")");
+                gas -= 1;
+            } else{
+                System.out.println("멈춥니다. (gas 잔량 : " + gas + ")");
+                return;
+            }
+        }
+    }
+}
