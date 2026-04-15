6a146222dd30075268c47b438ec02e4f2be9c0aa
leetaeshin <ts485482@gmail.com>
2026-04-15 16:19:15 +0900

    java

--- leetaeshin/src/ch06/sec06/PersonExample.java
+++ leetaeshin/src/ch06/sec06/PersonExample.java
@@ -8,8 +8,8 @@ public class PersonExample {
         System.out.println(p1.ssn);
         System.out.println(p1.name);
 
-        //p1.nation = "usa";                //컴파일 에러 (final로 값변경을 막음)
-        //p1.ssn = "654321-7654321";        //컴파일 에러 (final로 값변경을 막음)
+        //p1.nation = "usa";                //컴파일 에러 (Person 클래스에서 nation 객체 앞에 final을 달아 이후 값 변경을 막음)
+        //p1.ssn = "654321-7654321";        //컴파일 에러 (Person 클래스에서 ssn 객체 앞에 final을 달아 이후 값 변경을 막음)
         p1.name = "을지문덕";
     }
 }
