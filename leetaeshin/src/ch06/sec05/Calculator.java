83782e35b3c429ed6b6a97179287e4f8a5743b95
leetaeshin <ts485482@gmail.com>
2026-04-15 15:16:41 +0900

    java

---
+++ leetaeshin/src/ch06/sec05/Calculator.java
@@ -0,0 +1,15 @@
+package ch06.sec05;
+
+public class Calculator {
+    //정적 멤버 사용
+
+    static double pi = 3.1415923;
+
+    static int plus(int x, int y){
+        return x + y;
+    }
+
+    static int minus(int x, int y){
+        return x - y;
+    }
+}
