370d16930d0dbb05e2d59694fc0ee5a6c3bf5e03
leetaeshin <ts485482@gmail.com>
2026-04-16 11:22:53 +0900

    java

---
+++ leetaeshin/src/ch07/extend/DmbCellPhone.java
@@ -0,0 +1,26 @@
+package ch07.extend;
+
+public class DmbCellPhone extends CellPhone {
+    //자식 클래스
+    //필드
+    int channel;
+
+    //생성자
+    DmbCellPhone(String model, String color, int channel){
+        this.model = model;
+        this.color = color;
+        this.channel = channel;
+    }
+
+    //메소드
+    void turnOnBmb(){
+        System.out.println("채널 " + channel + "번 Dmb 방송 수신을 시작합니다.");
+    }
+    void changeChannelDmb(int channel) {
+        this.channel = channel;
+        System.out.println("채널 " + channel + "번으로 변경합니다.");
+    }
+    void turnOffDmb(){
+        System.out.println("DMB 방송 수신을 멈춥니다.");
+    }
+}
---
+++ leetaeshin/src/ch07/extend/DmbCellphoneExample.java
@@ -0,0 +1,29 @@
+package ch07.extend;
+
+public class DmbCellphoneExample {
+    public static void main(String[] args) {
+        //DmbCellphone객체 생성
+        DmbCellPhone dmbCellphone = new DmbCellPhone("자바폰", "검정", 10);
+
+        //CellPhone으로부터 상속 받은 필드
+        System.out.println("모델 : " + dmbCellphone.model);
+        System.out.println("색상 : " + dmbCellphone.color);
+
+        //DmbCellPhone의 필드
+        System.out.println("채널 : " + dmbCellphone.channel);
+
+        //CellPhone으로부터 상속 받은 메소드 호출
+        dmbCellphone.powerOn();
+        dmbCellphone.bell();
+        dmbCellphone.sendVoice("여보세요");
+        dmbCellphone.receiveVoice("안녕하세요! 저는 홍길동인데요");
+        dmbCellphone.sendVoice("아~ 예 반갑습니다.");
+        dmbCellphone.hangUp();
+
+        //DmbCellPhone의 메소드 호출
+        dmbCellphone.turnOnBmb();
+        dmbCellphone.changeChannelDmb(12);
+        dmbCellphone.turnOffDmb();
+        dmbCellphone.powerOff();
+    }
+}
