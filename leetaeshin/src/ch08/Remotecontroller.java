f94cd756cfe7fce4a36dc2ee7b2ed2db98537b0e
leetaeshin <ts485482@gmail.com>
2026-04-20 09:46:00 +0900

    java

--- leetaeshin/src/ch08/Remotecontroller.java
+++ leetaeshin/src/ch08/Remotecontroller.java
@@ -4,7 +4,14 @@ public class Remotecontroller {
     public static void main(String[] args) {
         Remotecontrol rc;
         rc = new Television();
+        rc.turnOn();
+        rc.setMute(true);
+        rc.turnOff();
+
         rc = new Audio();
+        rc.turnOn();
+        rc.setMute(true);
+        rc.turnOff();
 
         Remotecontrol.changeBattery();
     }
