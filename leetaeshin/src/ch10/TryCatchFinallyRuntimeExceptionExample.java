b2f2c197f69701ccfa43bd5b7fd545af91b0de6a
leetaeshin <ts485482@gmail.com>
2026-04-20 10:33:22 +0900

    java

---
+++ leetaeshin/src/ch10/TryCatchFinallyRuntimeExceptionExample.java
@@ -0,0 +1,32 @@
+package ch10;
+
+public class TryCatchFinallyRuntimeExceptionExample {
+    public static void main(String[] args) {
+        String data1 = null;
+        String data2 = null;
+        //try : 예외 발생 가능 코드 작성
+        try{
+            data1 = args[0];
+            data2 = args[1];
+        //catch(예외클래스 e) : 예외 발생 시 처리
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("실행 매개값의 수가 부족합니다.");
+            System.out.println("[실행 방법]");
+            System.out.println("java TryCatchFinallyRuntimeExceptionExample num1 num2");
+            return;
+        }
+
+        try{
+            int value1 = Integer.parseInt(data1);
+            int value2 = Integer.parseInt(data2);
+            int result = value1 + value2;
+            System.out.println(data1 + "+" + data2 + "=" + result);
+        } catch(NumberFormatException e){
+            System.out.println("숫자로 변환할 수 없습니다.");
+        } 
+        //finally : 예외 발생과 관계없이 항상 실행
+        finally{
+            System.out.println("다시 실행하세요.");
+        }
+    }
+}
